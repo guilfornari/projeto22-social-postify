eb9c80154df5ccac7bbb7aeac04caa1d10baa689
Guilfornari <guilfornari@gmail.com>
2023-08-26 19:21:57 -0300

    Created resource publication and added repository

---
+++ src/publications/publications.service.ts
@@ -0,0 +1,29 @@
+import { Injectable } from '@nestjs/common';
+import { CreatePublicationDto } from './dto/create-publication.dto';
+import { UpdatePublicationDto } from './dto/update-publication.dto';
+import { PublicationsRepository } from './publications.repository';
+
+@Injectable()
+export class PublicationsService {
+  constructor(private publicationRepository: PublicationsRepository) { }
+
+  create(createPublicationDto: CreatePublicationDto) {
+    return 'This action adds a new publication';
+  }
+
+  findAll() {
+    return `This action returns all publications`;
+  }
+
+  findOne(id: number) {
+    return `This action returns a #${id} publication`;
+  }
+
+  update(id: number, updatePublicationDto: UpdatePublicationDto) {
+    return `This action updates a #${id} publication`;
+  }
+
+  remove(id: number) {
+    return `This action removes a #${id} publication`;
+  }
+}
